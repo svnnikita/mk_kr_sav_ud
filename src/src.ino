e27da35436421c8314765ff2cc802b4ee17381de
svnnikita <svnnikitos@gmail.com>
2024-11-25 13:51:42 +0300

    Изменен подход к написанию программы, обновлен код

--- src/src.ino
+++ src/src.ino
@@ -1,47 +1,28 @@
-int signal_output = 10;
-int sec;
 
-float short_signal = 329.63;  // Ми
-float long_signal = 440.0;  // Ля
-
-int delayy_short = 500;
-int delayy_long = 600;
-
-unsigned long last_time = millis();
-
-void setup() {
-    pinMode(signal_output, OUTPUT);
-    Serial.begin(9600);
-
+void setup() {	
+	Serial.begin(9600);
 }
 
 void loop() {
-    if (Serial.available()) {
-        if (sec = Serial.parseInt()) {
-            for (byte i = 0; i < 3; i++) {
+	Serial.println("please, enter the time");
+	while (Serial.available() == 0) { // таймер ожидает ввода данных
+	}	
+	
+	int set = Serial.parseInt();
 
-                tone(signal_output, short_signal);
-                delay(delayy_short);
-
-                noTone(signal_output);
-                delay(delayy_short);
-
-            }
-
-            tone(signal_output, long_signal);
-            delay(delayy_long);
-            noTone(signal_output);
-        }
-
-        delay(sec);
-        tone(signal_output, long_signal);
-
-    }
-    else {
-        if (millis() - last_time > 1000) {
-            last_time = millis();
-            Serial.println("ожидание_времени");
-        }
-    };
-
+	if (set > 0) { // если пользователь ввел данные
+		int i = 0;
+		for ( ; i < 3; i++) {
+			tone(2, 440);
+			delay(1000);
+			noTone(2);
+			delay(1000);
+		}
+		if (i = 3) {
+			tone(2, 540);
+			delay(1000);
+			noTone(2);
+			delay(1000);
+		}
+	}
 }
