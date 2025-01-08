e085e83057134dc6fab7aa889d6ec4b3ea902164
svnnikita <svnnikitos@gmail.com>
2025-01-08 21:02:09 +0300

    Попытка реализовать таймер на несколько подходов

--- src/src.ino
+++ src/src.ino
@@ -29,18 +29,34 @@ unsigned long timing = 0;
 
 		flag3. Отвечает за работу таймера и за промежуточные сигналы динамика. Значения:
 	0 -- при значении нуля выполняется остальная часть программы. 
-	1 -- таймер заканчивает свою работу. Значения остальных флагов сбрасываются в ноль.
-*/
+	1 -- таймер заканчивает свою работу. Значения остальных флагов сбрасываются в ноль. */
 int flag1 = 0;
-int flag2 = 0;
-int flag3 = 0;
-int flag4 = 0;
+byte flag2 = 0;
+byte flag3 = 0;
+byte flag4 = 0;
+byte flag5 = 0;
+byte flag6 = 0;
+byte flag7 = 0;
+byte flag8 = 0;
 
-// Объявляем массив из 3-х значений: 
-// 1) количество подходов
-// 2) время одного подхода
-// 3) время паузы между подходами
+// Объявляем массив значений, где: 
+// элемент 1 -- количество подходов
+// элемент 2 -- время одного подхода
+// элемент 3 -- время паузы между подходами
 unsigned int arrayOfParam[3];
+
+// Создаем указатели на каждый элемент массива для управления таймером
+unsigned int* pNumOfAppr = &arrayOfParam[0]; 	// количество подходов
+unsigned int* pTimeOfAppr = &arrayOfParam[1];	// время одного подхода
+unsigned int* pTimeOfPause = &arrayOfParam[2];	// время паузы
+
+// Создаем массив из 2-х элементов и копируем значения
+// элементов 2 и 3 для дальнейшего взаимодействия
+unsigned int arrayOfParamCopy[2];
+
+// Создаем указатели на элементы массива-копии
+unsigned int* pTimeOfApprCopy = &arrayOfParamCopy[0];	// время одного подхода
+unsigned int* pTimeOfPauseCopy = &arrayOfParamCopy[1];	// время паузы
 
 void setup() {	
 	Serial.begin(9600);
@@ -50,7 +66,7 @@ void setup() {
 }
 
 void loop() {
-	if (millis() - timing > oneSec * 2 && flag1 == 0 && flag2 == 0 && flag3 == 0) {
+	if (millis() - timing > oneSec * 2 && flag1 == 0 && flag2 == 0) {
 		timing = millis();
 		onDisplay("ТАЙМЕР ДЛЯ", "ТРЕНИРОВОК");
 		flag1 = 1;
@@ -100,14 +116,14 @@ void loop() {
 		// w + число -- время одного подхода, второе значение в массиве arrayOfParam[]
 		case 'w':
 			onDisplay("ВРЕМЯ ОДНОГО", "ПОДХОДА:");
-			arrayOfParam[1] = Serial.parseInt();
+			arrayOfParam[1] = arrayOfParamCopy[0] = Serial.parseInt();
 			lcd.print(arrayOfParam[1]);
 			delay(oneSec * 2); 
 			break;
 		// e + число -- количество подходов, третье значение в массиве arrayOfParam[]
 		case 'e':
 			onDisplay("ВРЕМЯ", "ПАУЗЫ:");
-			arrayOfParam[2] = Serial.parseInt();
+			arrayOfParam[2] = arrayOfParamCopy[1] = Serial.parseInt();
 			lcd.print(arrayOfParam[2]);
 			delay(oneSec * 2); 
 			break;
@@ -145,63 +161,68 @@ void loop() {
 		flag2 = 1;
 	}
 
-	// Создаем указатели на каждый элемент массива для управления таймером
-	unsigned int* pNumOfAppr = &arrayOfParam[0]; 	// количество подходов
-	unsigned int* pTimeOfAppr = &arrayOfParam[1];	// время одного подхода
-	unsigned int* pTimeOfPause = &arrayOfParam[2];	// время паузы
-
 	// Начало работы таймера. Первый подход
-	if (millis() - timing > oneSec && flag2 == 1 && flag3 == 0) {
+	if (millis() - timing > oneSec && flag2 == 1 && flag3 == 0 && *pNumOfAppr > 0) {
 		timing = millis();
+		onDisplay("РАБОТАЕМ", *pTimeOfAppr);
+
 		*pTimeOfAppr = *pTimeOfAppr - 1;
 
-		/* При условии, что заданное время одного подхода кончилось,
-		флаг 3 устанавливается  единицу, и выполняется следующее условие - 
-		звучит сигнал о начале паузы */
 		if (*pTimeOfAppr == 0) {
 			flag3 = 1;
 		}
-
-		lcd.clear();
-		lcd.setCursor(0, 0);
-		lcd.print(*pTimeOfAppr);
-		}
+	}
 	
-	// Сигнал паузы. Флаг 3 устанавливается в положение 2, и начинается отсчет паузы
-	if (millis() - timing > oneSec && flag3 == 1) {
+	// Сигнал паузы.
+	if (millis() - timing > oneSec && *pTimeOfAppr == 0 && flag3 == 1 && flag4 == 0) {
 		tone(zummer, freq2, oneSec / 2);
-		flag3 = 2;
+		flag4 = 1;
 	}
 
-	// Начало паузы.
-	if (millis() - timing > oneSec && *pTimeOfAppr == 0 && flag3 == 2) {
+	// Начало паузы. 
+	if (millis() - timing > oneSec && *pTimeOfAppr == 0 && flag4 == 1 && flag5 == 0 && *pNumOfAppr > 0) {
 		timing = millis();
-
-		lcd.clear();
-		lcd.setCursor(0, 0);
-		lcd.print("ОТДЫХАЕМ");
-
+		onDisplay("ОТДЫХАЕМ", *pTimeOfPause);
+		
 		*pTimeOfPause = *pTimeOfPause - 1;
 
-		// По окончании паузы, если еще остались подходы, работа программы
-		// продолжается. Флаг 3 устанавливается в значение 3
-		if (*pTimeOfPause == 0 && *pNumOfAppr != 0) {
-			flag3 = 3;
+		// Условие окончания паузы
+		if (*pTimeOfPause == 0) {
+			*pTimeOfAppr = *pTimeOfAppr + *pTimeOfApprCopy;
+			*pTimeOfPause = *pTimeOfPause + *pTimeOfPauseCopy;
+
+			flag5 = 1; // условие "Начало паузы" больше не выполняется
 		}
-
-		lcd.setCursor(0, 1);
-		lcd.print(*pTimeOfPause);
-		}	
-		
-	if (millis() - timing > oneSec && flag3 == 3 && flag4 == 0) {
-		timing = millis();
-		flag2 == 1;
-		flag3 == 0;
-		//if () {
-
-		//}
-		//flag3 = 2;
 	}
+
+	// Сигнал окончания паузы
+	if (millis() - timing > oneSec && flag5 == 1 && flag6 == 0 && *pNumOfAppr > 0) {
+		tone(zummer, freq1, oneSec / 2);
+		flag2 = 1;
+		flag3 = 0;
+		flag5 = 0;
+
+		*pNumOfAppr--;
+	}
+
+	if (millis() - timing > oneSec && flag6 == 1 && flag7 == 0 && *pNumOfAppr == 0) {
+		flag1 = 0;
+		flag2 = 0;
+		flag3 = 0;			
+		flag4 = 0;
+		flag5 = 0;
+	}
+	/*
+	if (millis() - timing > oneSec && *pNumOfAppr == 0) {
+		//timing = millis();
+		flag1 = 0;
+		flag2 = 0;
+		flag3 = 0;			
+		flag4 = 0;
+		flag5 = 0;
+		flag6 = 0;
+		}
+	//}*/
 
 }
 
